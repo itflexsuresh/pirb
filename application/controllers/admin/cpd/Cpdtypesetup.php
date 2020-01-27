a4f97940e87a315cde3c3423cf7d98a08df07282
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-01-27 19:33:09 +0530

    CPD contoller sync

--- application/controllers/admin/cpd/Cpdtypesetup.php
+++ application/controllers/admin/cpd/Cpdtypesetup.php
@@ -6,7 +6,9 @@ class Cpdtypesetup extends CC_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		
 		$this->load->model('Cpdtypesetup_Model');
+		
 	}
 	
 	public function index($id='')
@@ -25,6 +27,17 @@ class Cpdtypesetup extends CC_Controller
 			$requestData 	= 	$this->input->post();
 
 			if($requestData['submit']=='submit'){
+				// QR CODE 
+
+				$SERVERFILEPATH 			= $_SERVER['DOCUMENT_ROOT'].'/auditit_new/pirbGit/assets/uploads/qrcode/';
+				$text 						= $requestData['productcode'];
+				$code_fileName 				= substr($text, 0,9);
+				$file_name 					= $code_fileName ."-Qrcode".rand(2,200).".png";
+				$Qrcode_path 					= $SERVERFILEPATH.$file_name;
+				define('IMAGE_WIDTH',1000);
+				define('IMAGE_HEIGHT',1000);
+				QRcode::png($text,$Qrcode_path,'L', '10', '10');
+				$requestData['qrcode']		= $file_name;
 				$data 	=  $this->Cpdtypesetup_Model->action($requestData);
 				if($data) $message = 'CPD Type '.(($id=='') ? 'created' : 'updated').' successfully.';
 			}else{
@@ -44,30 +57,30 @@ class Cpdtypesetup extends CC_Controller
 		$data['content'] 			= $this->load->view('admin/cpd/cpdtypesetup/index', (isset($pagedata) ? $pagedata : ''), true);
 		$this->layout2($data);
 	}
-	
+
 	public function DTCpdType()
 	{
 		$post 			= $this->input->post();
-		$totalcount 	= $this->Cpdtypesetup_Model->getList('count', ['status' => ['0','1']]+$post);
-		$results 		= $this->Cpdtypesetup_Model->getList('all', ['status' => ['0','1']]+$post);
+		$totalcount 	= $this->Cpdtypesetup_Model->getList('count', ['status' => ['1']]+$post);
+		$results 		= $this->Cpdtypesetup_Model->getList('all', ['status' => ['1']]+$post);
 		
 		$totalrecord 	= [];
 		if(count($results) > 0){
 			foreach($results as $result){
 				$totalrecord[] = 	[
-										'productcode' 		=> 	$result['productcode'],
-										'activity' 			=> 	$result['activity'],
-										'startdate' 		=> 	date('m-d-Y',strtotime($result['startdate'])),
-										'enddate' 			=> 	date('m-d-Y',strtotime($result['enddate'])),
-										'cpdstream' 		=> 	$this->config->item('cpdstream')[$result['cpdstream']],
-										'points' 			=> 	$result['points'],
+					'productcode' 		=> 	$result['productcode'],
+					'activity' 			=> 	$result['activity'],
+					'startdate' 		=> 	date('m-d-Y',strtotime($result['startdate'])),
+					'enddate' 			=> 	date('m-d-Y',strtotime($result['enddate'])),
+					'cpdstream' 		=> 	$this->config->item('cpdstream')[$result['cpdstream']],
+					'points' 			=> 	$result['points'],
 										//'status' 	=> 	$this->config->item('statusicon')[$result['status']],
-										'action'			=> 	'
-															<div class="table-action">
-																<a href="'.base_url().'admin/cpd/cpdtypesetup/index/'.$result['id'].'" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil-alt"></i></a>
-															</div>
-														'
-									];
+					'action'			=> 	'
+					<div class="table-action">
+					<a href="'.base_url().'admin/cpd/cpdtypesetup/index/'.$result['id'].'" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil-alt"></i></a>
+					</div>
+					'
+				];
 			}
 		}
 		
@@ -79,5 +92,63 @@ class Cpdtypesetup extends CC_Controller
 		);
 
 		echo json_encode($json);
+	}
+
+	public function getPDF($id){
+
+		if($id!=''){
+			$rowData = $this->Cpdtypesetup_Model->getList('row', ['id' => $id, 'status' => ['0','1']]);
+			if($rowData){
+				$fileName = $rowData['productcode'];
+
+				$html = '<!DOCTYPE html>
+				<html>
+				<head>
+				<title>CPD PDF</title>
+				</head>
+				<body>
+
+				<table style="width: 80%; display: table; margin: 0 auto; ">
+				<tbody>
+				<tr style="text-align: center;">
+				<td colspan="2"><img style="width: 200px;" src="https://lh3.googleusercontent.com/-RGusk-AC5bc/Xi7Nq5rWDQI/AAAAAAAAC44/gkq02n3UxR0ZmfszX9M0I80Tj4nc2-cTgCK8BGAsYHg/s0/2020-01-27.png"></td>
+				</tr>
+				<tr style="text-align: center;">
+				<td style="width: 50%; text-align: right;padding: 10px 20px 10px 0; font-weight: 700;">ACTIVITY NAME:</td>
+				<td style="text-align: left; padding: 10px 0 10px 0;">'.$rowData['activity'].'</td>
+				</tr>
+				<tr style="text-align: center;">
+				<td style="width: 50%; text-align: right; padding: 10px 20px 10px 0; font-weight: 700;">CPD POINTS FOR THIS ACTVITY:</td>
+				<td style="text-align: left; padding: 10px 0 10px 0;">'.$rowData['points'].'</td>
+				</tr>
+				<tr style="text-align: center;">
+				<td style="width: 50%; text-align: right; padding: 10px 20px 10px 0; font-weight: 700;">CPD STREAM:</td>
+				<td style="text-align: left; padding: 10px 0 10px 0;">'.$this->config->item('cpdstream')[$rowData['cpdstream']].'</td>
+				</tr>
+				<tr style="text-align: center;">
+				<td style="width: 50%; text-align: right; padding: 10px 20px 10px 0; font-weight: 700;">CPD ACTIVITY END DATE:</td>
+				<td style="text-align: left; padding: 10px 0 10px 0;">'.date('m-d-Y',strtotime($rowData['enddate'])).'</td>
+				</tr>
+				<tr style="text-align: center;">
+				<td style="width: 50%; text-align: right; padding: 10px 20px 10px 0; font-weight: 700;">CPD PRODUCT CODE:</td>
+				<td style="text-align: left; padding: 10px 0 10px 0;">'.$rowData['productcode'].'</td>
+				</tr>
+				<tr style="text-align: center;">
+				<td colspan="2">
+				<img style="width: 210px; padding-top: 20px;" src="'.$_SERVER['DOCUMENT_ROOT'].'/auditit_new/pirbGit/assets/uploads/qrcode/'.$rowData['qrcode'].'">
+				<p style="font-size: 12px">Use App Plumber to Scan this QR Code</p>
+				</td>
+				</tr>
+				</tbody>
+				</table>
+				</body>
+				</html>';
+				$pdfFilePath = "".$fileName.".pdf";
+				$this->pdf->loadHtml($html);
+				$this->pdf->setPaper('A4', 'portrait');
+				$this->pdf->render();
+				$this->pdf->stream($pdfFilePath);
+			}
+		}		
 	}
 }
