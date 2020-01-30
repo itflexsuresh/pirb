23c9ea1c5b2d0b1e575d9a72e233c420bfa231dc
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-01-30 12:06:38 +0530

    side bar sync

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -26,7 +26,7 @@ $flag = $userdata['flag'];
 					<li> 
 						<a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu">CPD</span></a>
 						<ul aria-expanded="false" class="collapse">
-							<li><a href="<?php echo base_url().'admin/cpd/cpdtypesetup'; ?>">Rates</a></li>							
+							<li><a href="<?php echo base_url().'admin/cpd/cpdtypesetup'; ?>">CPD Types</a></li>							
 						</ul>
 					</li>
 					<li> 
