4f8abb7a5766030646730c760646cccf77b92c68
arunprasath2808 <arunprasath@itflexsolutions.com>
2020-01-31 17:45:24 +0530

    Changed save and update button

--- application/views/auditor/profile/index.php
+++ application/views/auditor/profile/index.php
@@ -33,7 +33,7 @@
 
 	$areas 			= isset($result['areas']) ? $result['areas'] : [];
 
-	$heading 		= isset($result['id']) ? 'Save' : 'Update';   
+	$heading 		= isset($result['']) ? 'Save' : 'Update';   
 
 	$filepath 		= base_url().'assets/uploads/auditor/'.$id.'/';
 	$filepath1		= (isset($result['file1']) && $result['file1']!='') ? $filepath.$result['file1'] : base_url().'assets/images/auditor/profile.jpg';
