ee9b54428dcb450bfa9514e9fd3763e75ac67168
arunprasath2808 <arunprasath@itflexsolutions.com>
2020-01-31 10:46:14 +0530

    Controller have been replaced

--- application/core/CC_Controller.php
+++ application/core/CC_Controller.php
@@ -6,12 +6,9 @@ class CC_Controller extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('CC_Model');
 		$this->load->model('Users_Model');
 		$this->load->model('Installationtype_Model');
 		$this->load->model('Managearea_Model');
-		$this->load->model('Qualificationroute_Model');
-		$this->load->model('Rates_Model');
 	}
 	
 	public function layout1($data=[])
@@ -34,17 +31,7 @@ class CC_Controller extends CI_Controller
 		
 		if($type=='1'){
 			if($userDetails){
-				if($userDetails['type']=='1'){
-					redirect('admin/administration/installationtype'); 
-				}elseif($userDetails['type']=='3'){
-					if($userDetails['flag']=='1') redirect('plumber/profile/index'); 
-					else redirect('plumber/registration/index'); 
-				}elseif($userDetails['type']=='4'){
-					if($userDetails['flag']=='1') redirect('company/profile/index'); 
-					else redirect('company/registration/company'); 
-				}elseif($userDetails['type']=='5'){
-					redirect('auditor/profile/index'); 
-				}
+				redirect('admin/administration/installationtype'); 
 			}
 		}else{
 			if(!$userDetails){
@@ -58,7 +45,7 @@ class CC_Controller extends CI_Controller
 		$userDetails = $this->getUserDetails();
 		
 		if($userDetails){
-			return $userDetails['id'];
+			return $userDetails['u_id'];
 		}else{
 			return '';
 		}
@@ -68,7 +55,7 @@ class CC_Controller extends CI_Controller
 	{
 		if($this->session->has_userdata('userid')){
 			$userid = $this->session->userdata('userid');
-			$result = $this->Users_Model->getUserDetails('row', ['id' => $userid, 'status' => ['0','1']]);
+			$result = $this->Users_Model->getUserDetails('row', ['1'], $userid);
 			
 			if($result){
 				return $result;
@@ -100,39 +87,12 @@ class CC_Controller extends CI_Controller
 		if(count($data) > 0) return ['' => 'Select Province']+array_column($data, 'name', 'id');
 		else return [];
 	}
-	
-	public function getQualificationRouteList()
-	{
-		$data = $this->Qualificationroute_Model->getList('all', ['status' => ['1']]);
-		
-		if(count($data) > 0) return ['' => 'Select Qualification Route']+array_column($data, 'name', 'id');
-		else return [];
-	}
-	
-	public function getRates($id)
-	{
-		$data = $this->Rates_Model->getList('row', ['id' => $id, 'status' => ['1']]);
-		
-		if(count($data) > 0) return $data['amount'];
-		else return [];
-	}
-	
-	public function getPlumberRates()
-	{
-		return 	[
-					'1' => $this->getRates($this->config->item('learner')),
-					'2' => $this->getRates($this->config->item('assistant')),
-					'3' => $this->getRates($this->config->item('operator')),
-					'4' => $this->getRates($this->config->item('licensed'))
-				];
-	}
+
 	public function getCityList()
-{
-$data = $this->Auditor_Model->getListCity('all', ['status' => ['1']]);
-
-if(count($data) > 0) return ['' => 'Select City']+array_column($data, 'name', 'id');
-else return [];
-}
-
-
+	{
+		$data = $this->Managearea_Model->getListCity('all', ['status' => ['1']]);
+		
+		if(count($data) > 0) return ['' => 'Select City']+array_column($data, 'name', 'id');
+		else return [];
+	}
 }
