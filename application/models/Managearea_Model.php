fedcba2785fb2aeea7d5e7ec974d4699c13206dd
arunprasath2808 <arunprasath@itflexsolutions.com>
2020-01-31 12:18:58 +0530

    Replaced file

--- application/models/Managearea_Model.php
+++ application/models/Managearea_Model.php
@@ -4,10 +4,10 @@ class Managearea_Model extends CC_Model
 {
 	public function getList($type, $requestdata=[])
 	{
-		$this->db->select('t1.*,t2.id,t2.name city_name,t3.id,t3.name suburb');
-		$this->db->from('province t1');
-		$this->db->join('city t2','t2.province_id=t1.id','left');
-		$this->db->join('suburb t3','t3.province_id=t1.id','left');
+		$this->db->select('t1.*,t2.id,t2.name city_name,t3.id,t3.name province_name');
+		$this->db->from('suburb t1');
+		$this->db->join('city t2','t2.id=t1.province_id','left');
+		$this->db->join('province t3','t3.id=t1.city_id','left');
 		
 		if(isset($requestdata['id'])) 		$this->db->where('t1.id',$requestdata['id']);
 		if(isset($requestdata['status']))	$this->db->where_in('t1.status', $requestdata['status']);
@@ -39,68 +39,48 @@ class Managearea_Model extends CC_Model
 	
 	public function action($data)
 	{
+
 		$this->db->trans_begin();
-		
+
 		$userid			= 	$this->getUserID();
 		$id 			= 	$data['id'];
 		$datetime		= 	date('Y-m-d H:i:s');
 		
-		// $request		=	[
-		// 						'updated_at' 		=> $datetime,
-		// 						'updated_by' 		=> $userid
-		// 					];
-		$request1		=	[
-								'updated_at' 		=> $datetime,
-								'updated_by' 		=> $userid
-							];
-							$request2		=	[
-								'updated_at' 		=> $datetime,
-								'updated_by' 		=> $userid
-							];
-							
-							
-		if(isset($data['name'])) 	$request['name'] 	= $data['name'];
-		if(isset($data['status'])) 	$request['status'] 	= $data['status'];
+		if(isset($data['province']) && isset($data['city1']) && $data['city1']!=''){
+			$request1		=	[
+									'created_at' 		=> $datetime,
+									'created_by' 		=> $userid,
+									'updated_at' 		=> $datetime,
+									'updated_by' 		=> $userid,
+									'name'				=> $data['city1'],
+									'province_id'		=> $data['province'],
+									'status'			=> '1'
+								];
 
-		if(isset($data['city'])) 	$request1['name'] 	= $data['city'];
-        if(isset($data['status'])) 	$request1['status'] = $data['status'];
-
-        if(isset($data['suburb'])) 	$request2['name'] 	= $data['suburb'];
-        if(isset($data['status'])) 	$request2['status'] = $data['status'];
-
-		//$request['status'] 	= (isset($data['status'])) ? $data['status'] : '0';
-	
-		if($id==''){
-			 $request['id'] = $id;
-			// $request['created_by'] = $userid;
-			$this->db->insert('province', $request);
-			   $insert_id = $this->db->insert_id();
-
-					}else{
-			$this->db->update('province', $request, ['id' => $id]);
-		}
-
-		if($id==''){
-			$request1['created_at'] = $datetime;
-			$request1['created_by'] = $userid;
-			$request1['province_id'] =$insert_id ;
-			$this->db->insert('city', $request1);
-			$insert_id1 = $this->db->insert_id();
+			$result = $this->db->insert('city', $request1);
 		}else{
-			$this->db->update('city', $request1, ['id' => $id]);
-		}
 
-		if($id==''){
-			$request2['created_at'] = $datetime;
-			$request2['created_by'] = $userid;
-			$request2['province_id'] =$insert_id  ;
-			$request2['city_id'] =$insert_id1 ;
-			$this->db->insert('suburb', $request2);
-		}else{
-			$this->db->update('suburb', $request2, ['id' => $id]);
-		}
-			
-		if($this->db->trans_status() === FALSE)
+			$request2		=	[
+				'updated_at' 		=> $datetime,
+				'updated_by' 		=> $userid
+			];
+
+			if(isset($data['province'])) 	$request2['province_id'] 		= $data['province'];
+			if(isset($data['city'])) 		$request2['city_id'] 			= $data['city'];
+			if(isset($data['suburb'])) 		$request2['name'] 				= $data['suburb'];
+			if(isset($data['status'])) 		$request2['status'] 			= '1';
+	        
+			if($id==''){
+				$request2['created_at'] = $datetime;
+				$request2['created_by'] = $userid;
+
+				$result = $this->db->insert('suburb', $request2);
+			}else{
+				$result = $this->db->update('suburb', $request2, ['id' => $id]);
+			}
+		}	
+
+		if(!isset($result) && $this->db->trans_status() === FALSE)
 		{
 			$this->db->trans_rollback();
 			return false;
@@ -128,7 +108,6 @@ class Managearea_Model extends CC_Model
 						);
 
 		
-		
 		if(!$delete || $this->db->trans_status() === FALSE)
 		{
 			$this->db->trans_rollback();
@@ -141,7 +120,7 @@ class Managearea_Model extends CC_Model
 		}
 	}
 	
-	public function getProvinceList($type, $requestdata=[])
+	public function getListProvince($type, $requestdata=[])
 	{
 		$this->db->select('*');
 		$this->db->from('province');
@@ -158,6 +137,50 @@ class Managearea_Model extends CC_Model
 			elseif($type=='row') 	$result = $query->row_array();
 		}
 		
+		return $result;
+	}
+
+
+	public function getListCity($type, $requestdata=[])
+	{
+
+		$this->db->select('*');
+		$this->db->from('city');
+
+		if(isset($requestdata['id'])) 				$this->db->where('id', $requestdata['id']);
+		if(isset($requestdata['provinceid'])) 		$this->db->where('province_id', $requestdata['s']);
+		if(isset($requestdata['status']))			$this->db->where_in('status', $requestdata['status']);
+		
+		if($type=='count'){
+			$result = $this->db->count_all_results();
+			
+		}else{
+			$query = $this->db->get();
+			
+			if($type=='all') 		$result = $query->result_array();
+			elseif($type=='row') 	$result = $query->row_array();
+		}
+		return $result;
+	}
+
+	public function getListSuburb($type, $requestdata=[])
+	{
+		$this->db->select('*');
+		$this->db->from('suburb');
+
+		if(isset($requestdata['id'])) 				$this->db->where('id', $requestdata['id']);
+		if(isset($requestdata['provinceid'])) 		$this->db->where('province_id', $requestdata['provinceid']);
+		if(isset($requestdata['cityid'])) 			$this->db->where('city_id', $requestdata['cityid']);
+		if(isset($requestdata['status']))			$this->db->where_in('status', $requestdata['status']);
+		
+		if($type=='count'){
+			$result = $this->db->count_all_results();			
+		}else{
+			$query = $this->db->get();
+			
+			if($type=='all') 		$result = $query->result_array();
+			elseif($type=='row') 	$result = $query->row_array();
+		}
 		return $result;
 	}
 }
