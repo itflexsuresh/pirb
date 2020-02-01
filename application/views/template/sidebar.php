433268dedd768fd27bc03d57a35d8fc8de633faa
Ashok-PIRB <Ashok-PIRB>
2020-02-01 15:39:21 +0530

    To change menu style

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -23,7 +23,7 @@ $flag = $userdata['flag'];
 							<li><a href="<?php echo base_url().'admin/systemsetup/qualificationroutes/qualificationroute'; ?>">Qualification Routes</a></li>
 							<li><a href="<?php echo base_url().'admin/systemsetup/message/message'; ?>">Messages</a></li>
 							<li> 
-								<a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu">Performance Settings</span></a>
+								<a href="javascript:void(0)" aria-expanded="false">Performance Settings</a>
 								<ul aria-expanded="false" class="collapse">
 									<li><a href="<?php echo base_url().'admin/systemsetup/performancesettings/plumberperformance'; ?>">Plumber Performance Settings</a></li>
 								</ul>
