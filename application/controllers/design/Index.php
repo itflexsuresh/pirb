46bc8df60a970c7d446dd606b4fc93519953ad64
ITF user <rmanikandanrjsm@outlook.com>
2020-02-04 18:47:23 +0530

    Design Controller

---
+++ application/controllers/design/Index.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Index extends CC_Controller 
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function register()
+	{
+		$data['content'] 	= $this->load->view('design/register', (isset($pagedata) ? $pagedata : ''), true);
+		$this->layout2($data);
+	}
+}
