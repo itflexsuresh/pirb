71d66247a35e641cb335390d33cb19a9f585688f
arunprasath2808 <arunprasath@itflexsolutions.com>
2020-02-04 18:58:07 +0530

    Changed url in admin

--- application/core/CC_Controller.php
+++ application/core/CC_Controller.php
@@ -8,13 +8,10 @@ class CC_Controller extends CI_Controller
 		parent::__construct();
 		$this->load->model('CC_Model');
 		$this->load->model('Users_Model');
-		$this->load->model('Company_Model');
 		$this->load->model('Installationtype_Model');
 		$this->load->model('Managearea_Model');
 		$this->load->model('Qualificationroute_Model');
 		$this->load->model('Rates_Model');
-		$this->load->library('pdf');
-		$this->load->library('phpqrcode/qrlib');
 	}
 	
 	public function layout1($data=[])
@@ -34,20 +31,19 @@ class CC_Controller extends CI_Controller
 	public function middleware($type='')
 	{
 		$userDetails = $this->getUserDetails();
+		
 		if($type=='1'){
 			if($userDetails){
 				if($userDetails['type']=='1'){
 					redirect('admin/administration/installationtype'); 
 				}elseif($userDetails['type']=='3'){
-					if($userDetails['formstatus']=='1') redirect('plumber/profile/index'); 
+					if($userDetails['flag']=='1') redirect('plumber/profile/index'); 
 					else redirect('plumber/registration/index'); 
 				}elseif($userDetails['type']=='4'){
-					if($userDetails['formstatus']=='1') redirect('company/profile/index'); 
+					if($userDetails['flag']=='1') redirect('company/profile/index'); 
 					else redirect('company/registration/company'); 
 				}elseif($userDetails['type']=='5'){
 					redirect('auditor/profile/index'); 
-				}elseif($userDetails['type']=='6'){
-					redirect('resellers/profile/index'); 
 				}
 			}
 		}else{
@@ -57,18 +53,9 @@ class CC_Controller extends CI_Controller
 		}
 	}
 	
-	public function getPageStatus($pagestatus='')
+	public function getUserID()
 	{
-		if($pagestatus=='' || $pagestatus=='1'){
-			return '1';
-		}else{
-			return '0';
-		}
-	}
-	
-	public function getUserID($id='')
-	{
-		$userDetails = $this->getUserDetails($id);
+		$userDetails = $this->getUserDetails();
 		
 		if($userDetails){
 			return $userDetails['id'];
@@ -77,16 +64,11 @@ class CC_Controller extends CI_Controller
 		}
 	}
 	
-	public function getUserDetails($id='')
+	public function getUserDetails()
 	{
-		if($id!=''){
-			$userid = $id;
-		}elseif($this->session->has_userdata('userid')){
+		if($this->session->has_userdata('userid')){
 			$userid = $this->session->userdata('userid');
-		}
-		
-		if(isset($userid)){
-			$result = $this->Users_Model->getUserDetails('row', ['id' => $userid, 'status' => ['0','1','3']]);
+			$result = $this->Users_Model->getUserDetails('row', ['id' => $userid, 'status' => ['0','1']]);
 			
 			if($result){
 				return $result;
@@ -127,14 +109,6 @@ class CC_Controller extends CI_Controller
 		else return [];
 	}
 	
-	public function getCompanyList()
-	{
-		$data = $this->Company_Model->getList('all', ['status' => ['1']]);
-		
-		if(count($data) > 0) return ['' => 'Select Company']+array_column($data, 'company_name', 'id');
-		else return [];
-	}
-	
 	public function getRates($id)
 	{
 		$data = $this->Rates_Model->getList('row', ['id' => $id, 'status' => ['1']]);
@@ -154,7 +128,7 @@ class CC_Controller extends CI_Controller
 	}
 	public function getCityList()
 	{
-		$data = $this->Managearea_Model->getListCity('all', ['status' => ['1']]);
+		$data = $this->Auditor_Model->getListCity('all', ['status' => ['1']]);
 
 		if(count($data) > 0) return ['' => 'Select City']+array_column($data, 'name', 'id');
 		else return [];
