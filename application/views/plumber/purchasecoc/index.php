981133a8eab1f5bbdba95a3310bc9063496608f4
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-02-05 19:55:28 +0530

    coc purchase view sync

--- application/views/plumber/purchasecoc/index.php
+++ application/views/plumber/purchasecoc/index.php
@@ -154,9 +154,9 @@ $collectedbypirb 		= $collectedbypirb["amount"];
 					<!---	Payment	--->
 					<input id="merchant_id" name="merchant_id" value="10016054" type="hidden">
 					<input id="merchant_key" name="merchant_key" value="uwfiy08dfb6jn" type="hidden">
-					<input id="return_url" name="return_url" value="http://diyesh.com/auditit_new/pirb/return" type="hidden">
-					<input id="cancel_url" name="cancel_url" value="http://diyesh.com/auditit_new/pirb/cancel" type="hidden">
-					<input id="notify_url" name="notify_url" value="http://diyesh.com/auditit_new/pirb/notify" type="hidden">
+					<input id="return_url" name="return_url" value="<?php echo base_url().'plumber/purchasecoc/index/return'; ?>return" type="hidden">
+					<input id="cancel_url" name="cancel_url" value="<?php echo base_url().'plumber/purchasecoc/index/cancel'; ?>" type="hidden">
+					<input id="notify_url" name="notify_url" value="<?php echo base_url().'plumber/purchasecoc/index/notify'; ?>" type="hidden">
 					<input id="name_first" name="name_first" value="<?php echo $username['name']; ?>" type="hidden">
 					<input id="name_last" name="name_last" value="<?php echo $username['surname']; ?>" type="hidden">
 					<input id="email_address" name="email_address" value="<?php echo $username['email']; ?>" type="hidden">
