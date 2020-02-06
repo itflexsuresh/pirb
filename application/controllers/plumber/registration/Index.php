30ce744f5da35d2465995cfb65fdfee6da05412e
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-02-06 20:38:56 +0530

    test

--- application/controllers/plumber/registration/Index.php
+++ application/controllers/plumber/registration/Index.php
@@ -41,8 +41,8 @@ class Index extends CC_Controller
 		$pagedata['qualificationroute'] = $this->getQualificationRouteList();
 		$pagedata['plumberrates'] 		= $this->getPlumberRates();
 		$pagedata['company'] 			= $this->getCompanyList();
-		// $userdata1						= 	$this->Plumber_Model->getList('row', ['id' => $userid]);
-		// $data['desgnation']				= 	$userdata1;
+		$userdata1						= 	$this->Plumber_Model->getList('row', ['id' => $userid]);
+		$data['desgnation']				= 	$userdata1;
 		
 		$pagedata['titlesign'] 			= $this->config->item('titlesign');
 		$pagedata['gender'] 			= $this->config->item('gender');
