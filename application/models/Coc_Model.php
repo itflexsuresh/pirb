c64c739aa4d69306481f04461b8ce3da8d6b410b
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-02-07 20:45:42 +0530

    coc purchase

--- application/models/Coc_Model.php
+++ application/models/Coc_Model.php
@@ -3,10 +3,10 @@
 class Coc_Model extends CC_Model
 {
 	public function getCOCList($type, $requestdata){
-		 $result = $this->db
-		 			->select('*')
-		 			->from('paper_stock_management')
-		 			->where($requestdata);
+		$result = $this->db
+		->select('*')
+		->from('paper_stock_management')
+		->where($requestdata);
 		if ($type=='count') {
 			$result = $this->db->count_all_results();
 		}
@@ -16,29 +16,27 @@ class Coc_Model extends CC_Model
 		return $result;
 	}
 
-	public function action($requestdata){
-		$datetime		= 	date('Y-m-d H:i:s');
+	public function action($requestdata, $flag){
+		//$datetime		= 	date('Y-m-d H:i:s');
 
-		$result 		= $this->db->insert('coc_orders',$requestdata);
-		
-		if($requestdata['coc_type'] == 1) 		$requestdata2['type'] 			= 'electronic';
-		if($requestdata['coc_type'] == 2) 		$requestdata2['type'] 			= 'paper';
-		if(isset($requestdata['user_id'])) 		$requestdata2['user_id'] 		= $requestdata['user_id'];
-												$requestdata2['coc_status']		= 'admin_stock';
-												$requestdata2['audit_status']	=  NULL;
-												$request['purchased_at'] 		= $datetime;
-
-		for ($x = 1; $x <= $requestdata['coc_purchase']; $x++) {
-
-    		$stock 			= $this->db->insert('paper_stock_management',$requestdata2);
-
-		}
-
-		if ($result && $stock) {
-			return '1';
+		if ($flag == 1) {
+			$result 		= $this->db->insert('invoice',$requestdata);
+			$inv_id 		= $this->db->insert_id();
+			return $inv_id;
 		}else{
-			return '0';
+			if($requestdata['coc_type'] == 1) 		$requestdata2['type'] 			= 'electronic';
+			if($requestdata['coc_type'] == 2) 		$requestdata2['type'] 			= 'paper';
+			if(isset($requestdata['user_id'])) 		$requestdata2['user_id'] 		= $requestdata['user_id'];
+			$result 		= $this->db->insert('coc_orders',$requestdata);
+			if ($result) {
+				return '1';
+			}else{
+				return '0';
+			}
+
 		}
+		
+
 	}
 
 	public function ajaxOTP($requestdata){
@@ -48,13 +46,13 @@ class Coc_Model extends CC_Model
 
 	public function OTPVerification($requestdata){
 		$result = $this->db->select('*')
-							->from('otp')
-							->where('user_id',$requestdata['user_id'])
-							->where('otp',$requestdata['otp'])
-							->order_by('id', 'DESC')
-							->limit(1)
-							->get()
-							->row_array();
+		->from('otp')
+		->where('user_id',$requestdata['user_id'])
+		->where('otp',$requestdata['otp'])
+		->order_by('id', 'DESC')
+		->limit(1)
+		->get()
+		->row_array();
 		if ($result) {
 			return '1';
 		}else{
@@ -66,10 +64,10 @@ class Coc_Model extends CC_Model
 	// public function action($data)
 	// {	
 	// 	$this->db->trans_begin();
-		
+
 	// 	$userid			= 	$this->getUserID();
 	// 	$datetime		= 	date('Y-m-d H:i:s');
-		
+
 
 
 	// 	if(isset($data['name'])) 	         $request['name'] 	    =    $data['name'];
@@ -83,15 +81,15 @@ class Coc_Model extends CC_Model
 	// 	if(isset($data['regnumber'])) 		$request['reg_no'] 		= 	 $data['regnumber'];
 	// 	if(isset($data['vat'])) 		    $request['vat_no'] 	    = 	 $data['vat'];
 	// 	if(isset($data['comp_photo'])) 		$request['file2'] 		= 	 $data['comp_photo'];
-		
-		
+
+
 	// 	if(isset($data['billingaddress'])) 	$request1['address'] 	= 	 $data['billingaddress'];
 	// 	if(isset($data['province'])) 		$request1['province'] 	= 	 $data['province'];
 	// 	if(isset($data['city'])) 			$request1['city'] 		= 	 $data['city'];
 	// 	if(isset($data['suburb'])) 	 		$request1['suburb']     = 	 $data['suburb'];
 	// 	if(isset($data['postalcode'])) 	    $request1['postal_code']=    $data['postalcode'];
 
-		
+
 	// 	if(isset($data['bankname'])) 		$request2['bank_name'] 	=    $data['bankname'];
 	// 	if(isset($data['accountname'])) 	$request2['account_name']=   $data['accountname'];
 	// 	if(isset($data['branchcode'])) 		$request2['branch_code'] =   $data['branchcode'];
@@ -105,31 +103,31 @@ class Coc_Model extends CC_Model
 
 
 	// 		if(isset($request)){
-			
+
 	// 		$request['user_id'] 	= $userid;
 	// 			$audior_details = $this->db->insert('users_detail', $request);
 	// 		}
 
-			
+
 
 	// 		if(isset($request1)){
-			
+
 	// 		$request1['user_id'] 	= $userid;
 	// 			$audior_details = $this->db->insert('users_address', $request1);
 	// 		}
 
 	// 		if(isset($request2)){
-			
+
 	// 		$request2['user_id'] 	= $userid;
 	// 			$audior_details = $this->db->insert('users_bank', $request2);
 	// 		}
-			
+
 	// 		if(isset($request3)){
-			
+
 	// 		//$request3['user_id'] 	= $userid;
 	// 			$audior_details = $this->db->insert('users', $request3);
 	// 		}
-		
+
 
 	// 	if($this->db->trans_status() === FALSE)
 	// 	{
@@ -146,11 +144,11 @@ class Coc_Model extends CC_Model
 	public function checkcocpermitted($userid)
 	{
 		$query = $this->db
-				->select('coc_purchase_limit, coc_electronic')
-				->from('users_plumber')
-				->where('user_id', $userid)
-				->get()
-				->row_array();
+		->select('coc_purchase_limit, electronic_coc_log')
+		->from('users_plumber')
+		->where('user_id', $userid)
+		->get()
+		->row_array();
 
 		if($query){
 			return $query;
