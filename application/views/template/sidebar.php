199863e20d41a478a1d2f1c9bf84ced4b3e9fb6b
Ashok-PIRB <Ashok-PIRB>
2020-02-07 19:27:10 +0530

    To implement the UI Desigsn

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -34,10 +34,10 @@ $formstatus  = $userdata['formstatus'];
 					</li>
 
 					<li class="step coc_manage"> 
-						<a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu">Coc Management</span></a>
+						<a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu" style="white-space: nowrap;">Coc Management</span></a>
 						<ul aria-expanded="false" class="collapse">							
 							<li> 
-								<a class="sub_menu" href="javascript:void(0)" aria-expanded="false"><span class="hide-menu" style="white-space: nowrap;">Coc Management (Statement)</span></a>
+								<a class="sub_menu" href="javascript:void(0)" aria-expanded="false"><span class="hide-menu">Coc Management (Statement)</span></a>
 								<ul aria-expanded="false" class="collapse">
 									<li><a href="<?php echo base_url().''; ?>">Coc Management (Statement)</a></li>
 									<li><a href="<?php echo base_url().'admin/cocmanagement/cocmanagementstatement/coc_orders/index'; ?>">Coc Orders</a></li>
