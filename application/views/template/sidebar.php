cc5687f5d096b0b1a28edd8d753d52aa00518696
ITF user <rmanikandanrjsm@outlook.com>
2020-02-08 19:09:21 +0530

    sidebar

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -65,7 +65,12 @@ $formstatus  = $userdata['formstatus'];
 							<li><a href="<?php echo base_url().'admin/company/index'; ?>">Company</a></li>
 						</ul>
 					</li>
-					<li><a href="<?php echo base_url().'admin/resellers/index'; ?>">Resellers</a></li>
+					<li class="step five"> 
+						<a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu">Resellers</span></a>
+						<ul aria-expanded="false" class="collapse">
+							<li><a href="<?php echo base_url().'admin/resellers/index'; ?>">Resellers</a></li>
+						</ul>
+					</li>
 				<?php }elseif($type=='3'){ ?>
 					<li><a href="<?php echo base_url().'plumber/registration/index'; ?>">Dashboard</a></li>
 					<?php if($formstatus=='1'){ ?>
