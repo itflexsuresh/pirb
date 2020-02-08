77ad6bd8b317c6008c2a00197ec35166a7167d9d
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-02-08 15:53:44 +0530

    coc purchase

--- application/models/Coc_Model.php
+++ application/models/Coc_Model.php
@@ -24,8 +24,8 @@ class Coc_Model extends CC_Model
 			$inv_id 		= $this->db->insert_id();
 			return $inv_id;
 		}else{
-			if($requestdata['coc_type'] == 1) 		$requestdata2['type'] 			= 'electronic';
-			if($requestdata['coc_type'] == 2) 		$requestdata2['type'] 			= 'paper';
+			// if($requestdata['coc_type'] == 1) 		$requestdata2['type'] 			= 'electronic';
+			// if($requestdata['coc_type'] == 2) 		$requestdata2['type'] 			= 'paper';
 			if(isset($requestdata['user_id'])) 		$requestdata2['user_id'] 		= $requestdata['user_id'];
 			$result 		= $this->db->insert('coc_orders',$requestdata);
 			if ($result) {
