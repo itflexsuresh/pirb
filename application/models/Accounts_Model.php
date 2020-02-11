e8def344bd72ef705944e7bf632567c1ec966233
muthukumarmsc <60336678+muthukumarmsc@users.noreply.github.com>
2020-02-11 15:49:12 +0530

    data table checking type of list

--- application/models/Accounts_Model.php
+++ application/models/Accounts_Model.php
@@ -6,12 +6,12 @@ class Accounts_Model extends CC_Model
 	{
 		
         $query=$this->db->select('t1.*,t1.status,t1.created_at,
-        	t2.inv_id, t2.total_due, t2.quantity, t2.cost_value, t2.delivery_cost, t2.total_due, t3.reg_no, t3.id, t3.name name, t3.surname surname, t3.company_name company_name, t3.vat_no vat_no, t3.email2, t3.home_phone, t4.address, t4.suburb, t4.city');
+        	t2.inv_id, t2.total_due, t2.quantity, t2.cost_value, t2.delivery_cost, t2.total_due, t3.reg_no, t3.id, t3.name name, t3.surname surname, t3.company_name company_name, t3.vat_no vat_no, t3.email2, t3.home_phone,t4.type=1, t4.address, t4.suburb, t4.city');
 
         $this->db->from('invoice t1');
         $this->db->join('coc_orders t2','t2.user_id = t1.user_id', 'left');
         $this->db->join('users_detail t3', 't3.user_id = t1.user_id', 'left');
-        $this->db->join('users_address t4', 't4.user_id = t1.user_id', 'left');
+        $this->db->join('users_address t4', 't4.user_id = t1.user_id AND t4.type=1', 'left');
    
        if(isset($requestdata['id'])) $this->db->where('t1.inv_id', $requestdata['id']);
        
