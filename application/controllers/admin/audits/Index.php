79dbf4e2ed6a023b4b82c7ccefc50fe78ef2ba76
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-02-20 17:13:24 +0530

    auditor controller sync

--- application/controllers/admin/audits/Index.php
+++ application/controllers/admin/audits/Index.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Index extends CC_Controller 
 {
+	//////////////////
 	public function __construct()
 	{
 		parent::__construct();
