8dd419effe60454ea38d1f48d5630632dda863d8
arunprasath2808 <arunprasath@itflexsolutions.com>
2020-02-20 14:01:09 +0530

    My report listings in auditor

--- application/controllers/auditor/reportlisting/Index.php
+++ application/controllers/auditor/reportlisting/Index.php
@@ -6,41 +6,38 @@ class Index extends CC_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('Reportlist_Model');
+		$this->load->model('Auditor_Reportlisting_Model');
+		$this->load->model('Installationtype_Model');
+		$this->load->model('Subtype_Model');
 	}
 	
 	public function index($id='')
-	{
-
+	{ 
+	
 		if($id!=''){
-			$result = $this->Reportlist_Model->getList('row', ['id' => $id, 'status' => ['0','1']]);
+			
+			$result = $this->Auditor_Reportlisting_Model->getList('row', ['id' => $id, 'status' => ['0','1']]);
+			
 			if($result){
 				$pagedata['result'] = $result;
 			}else{
 				$this->session->set_flashdata('error', 'No Record Found.');
-				//redirect('plumber/registration'); 
+				redirect('auditor/reportlisting/index'); 
 			}
 		}
 		
 		if($this->input->post()){
-
 			$requestData 	= 	$this->input->post();
-
-			if($requestData['submit'] == 'submit')
-
-			{	
-
-				$data 	=  $this->Reportlist_Model->action($requestData);
-
+			$id				=	$requestData['id'];			
+			if($requestData['submit']=='submit'){
 				
-				if($data) $message = 'Report '.(($id == '') ? 'created' : 'updated').' successfully.';
+				$data 	=  $this->Auditor_Reportlisting_Model->action($requestData);
+				
+				if($data) $message = 'Report Listings '.(($id=='') ? 'created' : 'updated').' successfully.';
+			}else{
+				$data 			= 	$this->Auditor_Reportlisting_Model->changestatus($requestData);
+				$message		= 	'Report Listings deleted successfully.';
 			}
-
-			// else
-			// {
-			// 	$data 			= 	$this->Reportlist_Model->changestatus($requestData);
-			// 	$message		= 	'Installation Type deleted successfully.';
-			// }
 
 			if(isset($data)) $this->session->set_flashdata('success', $message);
 			else $this->session->set_flashdata('error', 'Try Later.');
@@ -48,35 +45,33 @@ class Index extends CC_Controller
 			redirect('auditor/reportlisting/index'); 
 		}
 		
-		
-
 		$pagedata['notification'] 	= $this->getNotification();
-		//$pagedata['province'] 		= $this->getProvinceList();
-		//$pagedata['installationtype'] = $this->getInstallationTypeList();		
+		$pagedata['installationtypelist'] 	= $this->getInstallationTypeList(); 
 
-		$data['plugins']			= ['datatables', 'datatablesresponsive', 'sweetalert', 'validation','datepicker'];
+		$data['plugins']			= ['datatables', 'datatablesresponsive', 'sweetalert', 'validation'];
 		$data['content'] 			= $this->load->view('auditor/reportlisting/index', (isset($pagedata) ? $pagedata : ''), true);
 		$this->layout2($data);
 	}
 	
-	
-	
-	public function DTReportlist()
+	public function DTReportListing()
 	{
-		$post 			= $this->input->post();
-		$totalcount 	= $this->Reportlist_Model->getList('count', ['status' => ['0','1']]+$post);
-		$results 		= $this->Reportlist_Model->getList('all', ['status' => ['0','1']]+$post);
+		$post 			= $this->input->post();		
+		$totalcount 	= $this->Auditor_Reportlisting_Model->getList('count', ['status' => ['0','1']]+$post);
+		$results 		= $this->Auditor_Reportlisting_Model->getList('all', ['status' => ['0','1']]+$post);
+
 		
 		$totalrecord 	= [];
 		if(count($results) > 0){
 			foreach($results as $result){
 				$totalrecord[] = 	[
-										'name' 		=> 	$result['installationtype'],
-										'name' 		=> 	$result['subtype'],
-										//'status' 	=> 	$this->config->item('statusicon')[$result['status']],
+										'favour_name' 	  => $result['favour_name'],
+										'installation_id' => $result['insname'],
+										'subtype_id' 	  => $result['name'],
+										'comments'	  	  => $result['comments'],									
+										'status' 		  => $this->config->item('statusicon')[$result['status']],
 										'action'	=> 	'
 															<div class="table-action">
-																<a href="'.base_url().'auditor/reportlisting/index/index/'.$result['id'].'" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil-alt"></i></a>
+																<a href="'.base_url().'auditor/reportlisting/Index/index/'.$result['id'].'" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil-alt"></i></a>
 																<a href="javascript:void(0);" data-id="'.$result['id'].'" class="delete" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></a>
 															</div>
 														'
@@ -93,4 +88,6 @@ class Index extends CC_Controller
 
 		echo json_encode($json);
 	}
+
+	
 }
