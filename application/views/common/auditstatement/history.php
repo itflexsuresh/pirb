4c6f7d60b2c31bcefa80d19d250a82212ea4f83e
ITF user <rmanikandanrjsm@outlook.com>
2020-02-27 14:38:24 +0530

    h

--- application/views/common/auditstatement/history.php
+++ application/views/common/auditstatement/history.php
@@ -1,9 +1,20 @@
 <?php
-	if($roletype=='1'){
-		$heading = 'Manage Allocted Audits';
-	}else if($roletype=='3' || $roletype=='5'){
-		$heading = 'Audit Report';
-	}
+
+if($roletype=='1'){
+	$heading = 'Manage Allocted Audits';
+}else if($roletype=='3' || $roletype=='5'){
+	$heading = 'Audit Report';
+}
+
+$plumberid			= $result['user_id'];
+$auditorid			= $result['auditorid'];
+	
+$count 				= '';
+$total 				= '';
+$cautionary 		= '';
+$refixincomplete 	= '';
+$refixcomplete 		= '';
+$noaudit 			= '';
 ?>
 
 <div class="row page-titles">
@@ -25,6 +36,104 @@
 	<div class="col-12">
 		<div class="card">
 		
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Number Audits Done to Date</label>
+						<input type="text" class="form-control" value="<?php echo $count; ?>" disabled>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Total Number of Audit Findings</label>
+						<input type="text" class="form-control" value="<?php echo $total; ?>" disabled>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Cautionary Audit Findings</label>
+						<div class=" col-md-12">
+							<div class="row">
+								<input type="text" class="form-control col-md-7" value="<?php echo $cautionary; ?>" disabled>
+								<input type="text" class="form-control col-md-4 offset-md-1" value="<?php echo $cautionary; ?>" disabled>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Refix (In-Complete) Audit Findings</label>
+						<div class=" col-md-12">
+							<div class="row">
+								<input type="text" class="form-control col-md-7" value="<?php echo $refixincomplete; ?>" disabled>
+								<input type="text" class="form-control col-md-4 offset-md-1" value="<?php echo $refixincomplete; ?>" disabled>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Refix (Complete) Audit Findings</label>
+						<div class=" col-md-12">
+							<div class="row">
+								<input type="text" class="form-control col-md-7" value="<?php echo $refixcomplete; ?>" disabled>
+								<input type="text" class="form-control col-md-4 offset-md-1" value="<?php echo $refixcomplete; ?>" disabled>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>No Audit Findings Audit Findings</label>
+						<div class=" col-md-12">
+							<div class="row">
+								<input type="text" class="form-control col-md-7" value="<?php echo $noaudit; ?>" disabled>
+								<input type="text" class="form-control col-md-4 offset-md-1" value="<?php echo $noaudit; ?>" disabled>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="table-responsive m-t-40">
+				<table class="table table-bordered table-striped datatables fullwidth">
+					<thead>
+						<tr>
+							<th>Audit Date</th>
+							<th>Auditor</th>
+							<th>Installatation Type</th>
+							<th>Sub Type</th>
+							<th>Statements</th>
+							<th>Audit Finding</th>
+						</tr>
+					</thead>
+				</table>
+			</div>
+			
 		</div>
 	</div>
 </div>		
+
+
+<script>
+	var auditorid = '<?php echo $auditorid; ?>';
+	var plumberid = '<?php echo $plumberid; ?>';
+	
+	$(function(){
+		
+		var options = {
+			url 	: 	'<?php echo base_url()."ajax/index/ajaxdtaudithistory"; ?>',
+			data	:	{auditorid : auditorid, plumberid : plumberid},
+			columns : 	[
+				{ "data": "date" },
+				{ "data": "auditor" },
+				{ "data": "installationtype" },
+				{ "data": "subtype" },
+				{ "data": "statementname" },
+				{ "data": "finding" }
+			]
+		};
+		
+		ajaxdatatables('.datatables', options);
+	});
+</script>
