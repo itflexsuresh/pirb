c22c81fa80797c8222cc726124ae2d5632839aa7
ITF user <rmanikandanrjsm@outlook.com>
2020-02-27 10:14:46 +0530

    ar

--- application/views/pdf/auditreport.php
+++ application/views/pdf/auditreport.php
@@ -1,3 +1,86 @@
+<style type="text/css">
+table.page_overall_auditreport {
+    margin: 10px auto;
+}
+table.head-all {
+    width: 100%;
+    border-bottom: 1.5px solid;    
+    margin-bottom: 20px;
+    padding-bottom: 20px;    
+}
+table.coc_details_overall {
+    width: 100%;
+    border-bottom: 1.5px solid;   
+    margin-bottom: 20px;
+    padding-bottom: 20px;    
+}
+table.coc_details_overall h3 {
+    border-bottom: 1.5px solid #000;
+    display: inline-block;
+    margin: 0;
+    margin-bottom: 10px;
+}	
+table.coc_details_overall td, table.auditor_details_overall td {
+    padding: 5px;
+    width: 33.3%;
+}
+table.page_overall_auditreport input[type="text"] {
+    padding: 5px;
+}
+table.auditor_details_overall h3 {
+    border-bottom: 1.5px solid #000;
+    display: inline-block;
+    margin: 0;
+    margin-bottom: 10px;
+}
+
+table.auditor_details_overall {
+    width: 100%;
+    border-bottom: 1.5px solid #000;
+    margin-bottom: 20px; 
+    padding-bottom: 20px;       
+}
+table.notice-license-text {
+    width: 100%;
+    border: 1px solid #000;
+    border-collapse: collapse;
+    margin-bottom: 25px; 
+}
+table.notice-license-text th {
+    background: #f00;
+}
+table.notice-license-text th h3 {
+    color: #fff;
+    font-weight: 600;
+    margin: 0;
+    padding: 5px 0;
+}
+table.table.table-bordered.reviewtable {
+    width: 100%;
+    border-collapse: collapse;
+}
+h3.audit-table-heading {
+    border-bottom: 1.5px solid #000;
+    display: inline-block;
+    margin: 20px 0 20px;
+}
+table.table.table-bordered.reviewtable td {
+    border: 1px solid #000;
+    padding: 10px;
+    text-align: center;
+}
+table.table.table-bordered.reviewtable th {
+    border: 1px solid #000;
+	padding: 15px;    
+}
+table.coc_details_overall label, table.auditor_details_overall label {
+    font-weight: bold;
+}
+table.notice-license-text tbody tr td {
+    padding: 5px;
+}
+</style>
+
 <?php
 $cocid 					= isset($result['id']) ? $result['id'] : '';
 	
@@ -15,100 +98,143 @@ $auditormobile 			= isset($result['auditormobile']) ? $result['auditormobile'] :
 $auditoremail 			= isset($result['auditoremail']) ? $result['auditoremail'] : '';
 ?>
 
-<div class="">
-	<div class="">
-		<h2>PIRB AUDIT REVIEW REPORT</h2>
-		<img src="<?php echo base_url().'assets/images/pitrb-logo.png'; ?>">
-	</div>
-	
-	<h3>COC DETAILS</h3>
-	<div class="">					
-		<div class="">
-			<label>Certificate No</label>
-			<input type="text" value="<?php echo $cocid; ?>">
-		</div>					
-		<div class="">			
-			<label>Plumbing Work Completion Date</label>
-			<input type="text" value="<?php echo $completiondate; ?>">
-		</div>						
-		<div class="">			
-			<label>Owners Name</label>
-			<input type="text" value="<?php echo $name; ?>">
-		</div>								
-		<div class="">			
-			<label>Name of Complex/Flat (if applicable)</label>
-			<input type="text" value="<?php echo $address; ?>">
-		</div>								
-		<div class="">			
-			<label>Street</label>
-			<input type="text" value="<?php echo $street; ?>">
-		</div>								
-		<div class="">			
-			<label>Number</label>
-			<input type="text" value="<?php echo $number; ?>">
-		</div>							
-		<div class="">			
-			<label>Province</label>
-			<input type="text" value="<?php echo $province; ?>">
-		</div>							
-		<div class="">			
-			<label>City</label>
-			<input type="text" value="<?php echo $city; ?>">
-		</div>							
-		<div class="">			
-			<label>Suburb</label>
-			<input type="text" value="<?php echo $suburb; ?>">
-		</div>	
-	</div>
-	
-	<h3>AUDITOR DETAILS</h3>
-	<div class="">							
-		<div class="">			
-			<label>Auditors Name and Surname</label>
-			<input type="text" value="<?php echo $auditorname; ?>">
-		</div>	
-		<div class="">			
-			<label>Phone (Mobile)</label>
-			<input type="text" value="<?php echo $auditormobile; ?>">
-		</div>	
-		<div class="">			
-			<label>Email</label>
-			<input type="text" value="<?php echo $auditoremail; ?>">
-		</div>	
-	</div>
-	
-	<div>
-		<h3>NOTICE TO LICENSED PLUMBER</h3>
-		<p>It is your responsible to complete your refix's with in the allocted time. Failure to do so within the alloated time will result in the refix being marked as Audit Complete (with Refix(s)) and relevant remedial action will follow.</p>
-		<p>Please login into your PIRB Profile for further details pertaining to this audit review.</p>
-	</div>
-	
-	<h3>AUDIT REVIEW</h3>
-	<table class="table table-bordered reviewtable">								
+<table class="page_overall_auditreport">
+	<tbody>
 		<tr>
-			<th>Review Type</th>
-			<th>Statement</th>
-			<th>SANS/Regulation/Bylaw Reference</th>
-			<th>Comments</th>
-			<th>Images</th>
+			<td>
+				<table class="head-all">
+					<tbody>
+						<tr>
+							<td><h2>PIRB AUDIT REVIEW REPORT</h2></td>
+							<td><img src="<?php echo base_url().'assets/images/pitrb-logo.png'; ?>"></td>                   
+						</tr>
+					</tbody>
+				</table>
+			</td>
 		</tr>
-		<?php foreach($reviewlist as $list){ ?>
-			<tr>
-				<td><?php echo isset($this->config->item('reviewtype')[$list['reviewtype']]) ? $this->config->item('reviewtype')[$list['reviewtype']] : ''; ?></td>
-				<td><?php echo $list['statementname']; ?></td>
-				<td><?php echo $list['reference']; ?></td>
-				<td><?php echo $list['comments']; ?></td>
-				<td>
-					<?php 
-						$filelist = array_filter(explode(',', $list['file'])); 
-						foreach($filelist as $file){
-					?>
-							<img src="<?php echo base_url().'assets/uploads/auditor/statement/'.$file; ?>" width="50">
-					<?php
-						}
-					?>
-				</td>
-			</tr>
-		<?php } ?>
-	</table>
-</div>
+
+		<tr> 
+			<td>
+				<table class="coc_details_overall">
+					<tbody>
+						<tr> 
+							<td style="padding: 0;"> <h3>COC DETAILS</h3> </td>
+						</tr>
+						<tr>
+							<td style="padding: 0;"><label>Certificate No</label></td>
+							<td><label>Plumbing Work Completion Date</label></td>
+							<td><label>Owners Name</label></td>
+						</tr>
+						<tr>
+							<td style="padding: 0;"><input type="text" value="<?php echo $cocid; ?>"></td>
+							<td><input type="text" value="<?php echo $completiondate; ?>"></td>
+							<td><input type="text" value="<?php echo $name; ?>"></td>
+						</tr> 
+						<tr>
+							<td style="padding: 0;"><label>Name of Complex/Flat (if applicable)</label></td>
+							<td><label>Street</label></td>
+							<td><label>Number</label></td>
+						</tr>
+						<tr>
+							<td style="padding: 0;"><input type="text" value="<?php echo $address; ?>"></td>
+							<td><input type="text" value="<?php echo $street; ?>"></td>
+							<td><input type="text" value="<?php echo $number; ?>"></td>
+						</tr>
+						<tr>
+							<td style="padding: 0;"><label>Province</label></td>
+							<td><label>City</label></td>
+							<td><label>Suburb</label></td>
+						</tr>
+						<tr>
+							<td style="padding: 0;"><input type="text" value="<?php echo $province; ?>"></td>
+							<td><input type="text" value="<?php echo $city; ?>"></td>
+							<td><input type="text" value="<?php echo $suburb; ?>"></td>
+						</tr>						
+					</tbody>
+				</table>
+			</td>
+		</tr>    
+
+		<tr>
+			<td>
+				<table class="auditor_details_overall">
+					<tbody>
+						<tr>
+							<td style="padding: 0;"><h3>AUDITOR DETAILS</h3></td>
+						</tr>						
+						<tr>
+							<td style="padding: 0;"><label>Auditors Name and Surname</label></td>
+							<td><label>Phone (Mobile)</label></td>
+							<td><label>Email</label></td>
+						</tr>
+						<tr>
+							<td style="padding: 0;"><input type="text" value="<?php echo $auditorname; ?>"></td>
+							<td><input type="text" value="<?php echo $auditormobile; ?>"></td>
+							<td><input type="text" value="<?php echo $auditoremail; ?>"></td>                   
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
+
+		<tr>
+			<td style="border-bottom: 1.5px solid #000">
+				<table class="notice-license-text">
+					<thead>
+						<tr>
+						<th><h3>NOTICE TO LICENSED PLUMBER</h3></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>It is your responsible to complete your refix's with in the allocted time. Failure to do so within the alloated time will result in the refix being marked as Audit Complete (with Refix(s)) and relevant remedial action will follow.</td>             
+						</tr>
+
+						<tr>
+							<td>Please login into your PIRB Profile for further details pertaining to this audit review.</td>               
+						</tr>
+
+					</tbody>
+				</table>
+			</td>
+		</tr>
+		
+		<tr>
+			<td>
+			<h3 class="audit-table-heading">AUDIT REVIEW</h3>
+				<table class="table table-bordered reviewtable">    
+					<thead>
+						<tr>
+							<th>Review Type</th>
+							<th>Statement</th>
+							<th>SANS/Regulation/Bylaw Reference</th>
+							<th>Comments</th>
+							<th>Images</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<?php foreach($reviewlist as $list){ ?>
+							<tr>
+								<td><?php echo isset($this->config->item('reviewtype')[$list['reviewtype']]) ? $this->config->item('reviewtype')[$list['reviewtype']] : ''; ?></td>
+								<td><?php echo $list['statementname']; ?></td>
+								<td><?php echo $list['reference']; ?></td>
+								<td><?php echo $list['comments']; ?></td>
+								<td>
+									<?php 
+										$filelist = array_filter(explode(',', $list['file'])); 
+										foreach($filelist as $file){
+									?>
+											<img src="<?php echo base_url().'assets/uploads/auditor/statement/'.$file; ?>" width="50">
+									<?php
+										}
+									?>
+								</td>
+							</tr>
+						<?php } ?>      
+					</tbody>                            
+				</table>
+			</td>
+		</tr>
+	</tbody>
+</table>
