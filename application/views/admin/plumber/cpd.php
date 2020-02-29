0400987b8a7d2a5684cf3c543a01b200509dcce9
selvamanim <selvamani@itflexsolutions.com>
2020-02-29 19:06:01 +0530

    cpd

--- application/views/admin/plumber/cpd.php
+++ application/views/admin/plumber/cpd.php
@@ -8,6 +8,7 @@ if($roletype=='1'){
 
 $plumberid			= '';
 $auditorid			= '';
+$developmental1 = '';
 
 $cpdarray = explode("@@@", $settings_cpd[0]['cpd']);
 $devarray = explode("@-@", $cpdarray[0]);
