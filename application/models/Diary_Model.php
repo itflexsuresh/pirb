b37c05362342e0e911cf6b005405d53f190cb464
ITF user <rmanikandanrjsm@outlook.com>
2020-03-02 18:53:39 +0530

    c

--- application/models/Diary_Model.php
+++ application/models/Diary_Model.php
@@ -12,10 +12,10 @@ class Diary_Model extends CC_Model
 			concat(ud4.name, " ", ud4.surname) as auditorname,
 		');
 		$this->db->from('diary d');
-		$this->db->join('users_detail ud1', 'ud1.id = d.admin_id', 'left');
-		$this->db->join('users_detail ud2', 'ud2.id = d.plumber_id', 'left');
-		$this->db->join('users_detail ud3', 'ud3.id = d.company_id', 'left');
-		$this->db->join('users_detail ud4', 'ud4.id = d.auditor_id', 'left');
+		$this->db->join('users_detail ud1', 'ud1.user_id = d.admin_id', 'left');
+		$this->db->join('users_detail ud2', 'ud2.user_id = d.plumber_id', 'left');
+		$this->db->join('users_detail ud3', 'ud3.user_id = d.company_id', 'left');
+		$this->db->join('users_detail ud4', 'ud4.user_id = d.auditor_id', 'left');
 		
 		if(isset($requestdata['id'])) 			$this->db->where('d.id', $requestdata['id']);		
 		if(isset($requestdata['adminid'])) 		$this->db->where('d.admin_id', $requestdata['adminid']);
