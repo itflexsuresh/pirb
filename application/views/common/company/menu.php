88156d63899d17179b067cbef415efea911731b9
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-03-03 11:27:43 +0530

    menu company

--- application/views/common/company/menu.php
+++ application/views/common/company/menu.php
@@ -1,8 +1,7 @@
 
-
 <div class="card-body">
 	<div class="col-md-12 breadcrumb_tab">
-		<a href="<?php echo base_url().'admin/company/index/action/'.$id; ?>" class="stepbar">Plumbers Profile</a>
+		<a href="<?php echo base_url().'admin/company/index/action/'.$id; ?>" class="stepbar">Company Profile</a>
 		<a href="<?php echo base_url().'admin/company/index/emplist/'.$id; ?>" class="stepbar">Employee Listings</a>
 		<a href="<?php echo base_url().'admin/company/index/diary/'.$id; ?>" class="stepbar">Diary/Comments</a>
 	</div>
