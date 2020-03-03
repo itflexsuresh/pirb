effb8cc8351f0f779d2547a61ff7d50b57c3445d
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-03-03 11:06:38 +0530

    comapny emp listing

--- application/views/company/employee_listing.php
+++ application/views/company/employee_listing.php
@@ -187,8 +187,7 @@
 						</thead>
 					</table>
 				</div>
-				<?php if (isset($employee) && $employee!='') { ?>
-					<div class="row">
+				<div class="row">
 						<div class="col-md-5 align-self-center">
 						<h4 class="card-title app_status">Average Industry Rating of Company Employees</h4>
 						</div>
@@ -205,6 +204,8 @@
 						<input type="text" class="form-control" readonly name="">
 					</div>
 				</div>
+				<?php if (isset($employee) && $employee!='') { ?>
+
 				<div class="row">
 					<div class="col-md-6">
 						<h4 class="card-title">Employee Details</h4>
@@ -274,28 +275,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="row">
-					<div class="col-md-6">
-						<div class="form-group">
-							<label>Number of Logged COC's:</label>
-							<input type="text" class="form-control" readonly name=""  value="">
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-6">
-						<div class="form-group">
-							<label>Number Audits Done to Date:</label>
-							<input type="text" class="form-control" readonly name=""  value="">
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="form-group">
-							<label></label>
-							<input type="text" class="form-control" readonly name=""  value="">
-						</div>
-					</div>
-				</div>
+					
 					<h4 class="card-title">CPD Overview</h4>
 				<div id="reviewchart"></div>
 				
