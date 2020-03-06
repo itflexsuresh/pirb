be6a3d5f2ca63800c9ee656a32a9c22949d391ae
ITF user <rmanikandanrjsm@outlook.com>
2020-03-06 10:56:34 +0530

    cc

--- application/core/CC_Controller.php
+++ application/core/CC_Controller.php
@@ -275,7 +275,7 @@ class CC_Controller extends CI_Controller
 				$userorderstock 	= array_sum(array_column($orderquantity, 'quantity'));
 				$plumberstock		= ($userpaperstock + $userorderstock);
 				if($coclimit < $plumberstock){
-					$this->session->set_flashdata('error', 'Plumber already has '.$userpaperstock.' coc without logged.');
+					$this->session->set_flashdata('error', 'Plumber already has '.$userpaperstock.' coc without logged and '.$userorderstock.' coc waiting for approval.');
 					
 					if($extras['redirect']) redirect($extras['redirect']); 
 					else redirect('admin/plumber/index'); 
