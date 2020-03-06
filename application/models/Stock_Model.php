6f705a45c8797b6c7659a7026cb714b5b68c4068
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-03-06 16:09:25 +0530

    stock model

--- application/models/Stock_Model.php
+++ application/models/Stock_Model.php
@@ -236,7 +236,7 @@ class Stock_Model extends CC_Model
 
 		        $stringaarr = explode("@@@",$rowData['areas']);
 		        $provincesettings = explode("@@@",$rowData2['provincesettings']);
-		        $logo = FCPATH.'assets\images\pitrb-logo.png';
+		        $logo = base64conversion(FCPATH.'assets\images\pitrb-logo.png');
 
 
 
