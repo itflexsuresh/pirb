d2f7a623f89b5f5ee3872e8ef415913bf0cf98ea
ITF user <rmanikandanrjsm@outlook.com>
2020-03-07 19:29:57 +0530

    cc

--- application/views/common/auditstatement/menu.php
+++ application/views/common/auditstatement/menu.php
@@ -9,12 +9,14 @@ if($roletype=='1'){
 	$url 		= 'auditor/auditstatement/index/';
 	$reviewtype = ($auditcomplete=='1') ? 'view/' : 'action/';
 }
+
+$segment5 = $this->uri->segment(5);
 ?>
 
 <div class="col-md-12 auditmenu_wrapper">
 	<div class="row">
-		<div class="col-md-4 auditmenu"><a href="<?php echo base_url().$url.'history/'.$id; ?>">Plumber Audit History</a></div>
-		<div class="col-md-4 auditmenu"><a href="<?php echo base_url().$url.$reviewtype.$id; ?>">Audit Review</a></div>
-		<div class="col-md-4 auditmenu"><a href="<?php echo base_url().$url.'diary/'.$id; ?>">Diary/Comments</a></div>
+		<div class="col-md-4 auditmenu <?php if($segment5=='history'){ echo 'auditmenuactive';} ?>"><a href="<?php echo base_url().$url.'history/'.$id; ?>">Plumber Audit History</a></div>
+		<div class="col-md-4 auditmenu <?php if($segment5=='view' || $segment5=='action'){ echo 'auditmenuactive';} ?>"><a href="<?php echo base_url().$url.$reviewtype.$id; ?>">Audit Review</a></div>
+		<div class="col-md-4 auditmenu <?php if($segment5=='diary'){ echo 'auditmenuactive';} ?>"><a href="<?php echo base_url().$url.'diary/'.$id; ?>">Diary/Comments</a></div>
 	</div>
 </div>
