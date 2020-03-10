6b412d46e3d654309fae81e398126c4015a0270d
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-03-10 18:48:42 +0530

    copany model

--- application/models/Company_Model.php
+++ application/models/Company_Model.php
@@ -211,8 +211,8 @@ class Company_Model extends CC_Model
 		if(isset($data['approval_status'])) $request4['status']			= '1';
 		if(isset($data['companystatus']) && $data['companystatus']=='2') 	$request4['status'] 		= '2';
 		if(isset($data['companystatus']) && $data['companystatus']=='1') 	$request4['status'] 		= '1';
-		if(isset($data['companystatus']) && $data['companystatus']=='3') 	$request4['status'] 		= '0';
-		if(isset($data['companystatus']) && $data['companystatus']=='4') 	$request4['status'] 		= '0';
+		if(isset($data['companystatus']) && $data['companystatus']=='3') 	$request4['status'] 		= '2';
+		if(isset($data['companystatus']) && $data['companystatus']=='4') 	$request4['status'] 		= '2';
 		if(isset($request4)){
 			if(isset($data['user_id'])){
 				$userid = $data['user_id'];	
