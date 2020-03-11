a5993175107f530e73ec0d4c91fa8dc9c5571efd
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-03-11 10:26:14 +0530

    my cpd model

--- application/models/Mycpd_Model.php
+++ application/models/Mycpd_Model.php
@@ -8,7 +8,7 @@ class Mycpd_Model extends CC_Model
 		
 		$datetime = date('Y-m-d H:i:s');
 		if ($requestdata['pagestatus'] == '1') {
-		$this->db->select('t1.*, t2.renewal_date');
+		$this->db->select('t1.*, t2.renewal_date, t2.expirydate');
 		$this->db->from('cpd_activity_form t1');
 		$this->db->join('users t2', 't2.id = t1.user_id', 'left')->order_by('t1.id','DESC');
 
@@ -17,7 +17,7 @@ class Mycpd_Model extends CC_Model
 
 		if(isset($requestdata['user_id'][0])) $this->db->where('t2.id', $requestdata['user_id'][0]);
 
-		$this->db->where('t2.renewal_date<=','t1.created_at', false);
+		$this->db->where('t2.expirydate<=','t1.created_at', false);
 
 		}elseif ($requestdata['pagestatus'] == '0') {
 		$this->db->select('t1.*, t2.renewal_date');
@@ -28,7 +28,7 @@ class Mycpd_Model extends CC_Model
 		if(isset($requestdata['user_id'][0])) $this->db->where('t2.id', $requestdata['user_id'][0]);
 		if(isset($requestdata['id'])) $this->db->where('t1.id', $requestdata['id']);
 
-		$this->db->where('t2.renewal_date>=','t1.created_at', false);
+		$this->db->where('t2.expirydate>=','t1.created_at', false);
 		}
 
 
