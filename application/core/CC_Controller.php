27320e26cd5fbffb6cd57bd557f362d03f9d38a0
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-03-11 11:17:00 +0530

    cc controllers

--- application/core/CC_Controller.php
+++ application/core/CC_Controller.php
@@ -1138,14 +1138,14 @@ class CC_Controller extends CI_Controller
 		}
 	}
 
-	// public function cronLog($extras){
-	// 	$requestdata0['	filename'] 		= '';
-	// 	$requestdata0['	start_time'] 	= date('Y-m-d H:i:s');
-	// 	$result = $this->db->insert('cron_log',$requestdata0);
-	// 	$insert_id 	= $this->db->insert_id();
+	public function cronLog($filename){
+		$requestdata0['	filename'] 		= $filename;
+		$requestdata0['	start_time'] 	= date('Y-m-d H:i:s');
+		$result = $this->db->insert('cron_log',$requestdata0);
+		$insert_id 	= $this->db->insert_id();
 
-	// 	if ($result && $insert_i) {
-	// 		return $insert_id;
-	// 	}
-	// }
+		if ($result && $insert_id) {
+			return $insert_id;
+		}
+	}
 }
