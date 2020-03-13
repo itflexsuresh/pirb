35f47e23eebf804118b1a4b7e2e2615a74795ffb
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-03-13 18:25:27 +0530

    letter for company

--- application/views/common/company/menu.php
+++ application/views/common/company/menu.php
@@ -3,6 +3,7 @@
 	<div class="col-md-12 breadcrumb_tab">
 		<a href="<?php echo base_url().'admin/company/index/action/'.$id; ?>" class="stepbar">Company Profile</a>
 		<a href="<?php echo base_url().'admin/company/index/emplist/'.$id; ?>" class="stepbar">Employee Listings</a>
+		<a href="<?php echo base_url().'admin/company/index/documents/'.$id; ?>" class="stepbar">Documents/Letters</a>
 		<a href="<?php echo base_url().'admin/company/index/diary/'.$id; ?>" class="stepbar">Diary/Comments</a>
 	</div>
 </div>
