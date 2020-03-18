0ec532ba73ba8f6305fd1bdb0eadc1a005a8faa4
rmanikandarRJSM <rmanikandarjsm@outlook.com>
2020-03-18 18:54:59 +0530

    company

--- application/controllers/company/Employee_listing.php
+++ application/controllers/company/Employee_listing.php
@@ -23,15 +23,24 @@ class Employee_listing extends CC_Controller
             $pagedata['plumberstatus']  = $this->config->item('plumberstatus');
             $userdata1                  = $this->Plumber_Model->getList('row', ['id' => $result[0]['user_id']]);
            // print_r($userdata1);die;
-            $pagedata['user_details']   = $userdata1;
+            $pagedata['user_details1']  = $userdata1;
 
             
             
-            $pagedata['history']        = $this->Auditor_Model->getReviewHistory2Count(['auditorid' => '', 'plumberid' => $result[0]['user_id']]);
+            ////$pagedata['history']        = $this->Auditor_Model->getReviewHistory2Count(['plumberid' => $result[0]['user_id']]);
+            $pagedata['history']        = $this->Auditor_Model->getReviewHistoryCount(['plumberid' => $result[0]['user_id']]);
+
+            $pagedata['history2']       = $this->Auditor_Model->getReviewHistory2Count(['plumberid' => $result[0]['user_id']]);
+
+            $pagedata['logged']         = $this->Coc_Model->getCOCList('count', ['user_id' => $result[0]['user_id'], 'coc_status' => ['2']]);
+
+            $pagedata['allocated']      = $this->Coc_Model->getCOCList('count', ['user_id' => $result[0]['user_id'], 'coc_status' => ['4']]);
+
+            $pagedata['nonlogged']      = $this->Coc_Model->getCOCList('count', ['user_id' => $result[0]['user_id'], 'coc_status' => ['5']]);
+
+            $pagedata['user_details']   = $this->Plumber_Model->getList('row', ['id' => $result[0]['user_id']]);
+
             $pagedata['settings_cpd']   = $this->Systemsettings_Model->getList('all',['user_id' => $result[0]['user_id']]);
-            
-
-            $pagedata['loggedcoc']      = $this->Coc_Model->getCOCList('count', ['user_id' => $result[0]['user_id'], 'coc_status' => ['2']]);
         }
 
 		$companyID = $this->getuserID();
