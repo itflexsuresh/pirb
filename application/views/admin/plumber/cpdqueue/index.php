581a23d6bbd3f726ec33c2954b1fccc02bad881d
manikandank228 <mr.manikandan01@gmail.com>
2020-03-27 17:25:11 +0530

    sdfsda

--- application/views/admin/plumber/cpdqueue/index.php
+++ application/views/admin/plumber/cpdqueue/index.php
@@ -252,24 +252,7 @@ $heading 				= isset($result['id']) ? 'Update' : 'Add';
 
 		fileupload([".document_file_ad", "./assets/uploads/cpdqueue", ['jpg','gif','jpeg','png','pdf','tiff','tif']], ['.document_picture_ad', '.document_image_ad', filepath, pdfimg]);
 		
-		// var options = {
-		// 	url 	: 	'<?php // echo base_url()."admin/cpd/cpdtypesetup/DTCpdQueue"; ?>',
-		// 	columns : 	[
-		// 	{ "data": "date" },
-		// 	{ "data": "namesurname" },
-		// 	{ "data": "reg_number" },
-		// 	{ "data": "acivity" },
-		// 	{ "data": "points" },
-		// 	{ "data": "status" },
-		// 	{ "data": "action" }
-		// 	],
-		// 	data : {pagestatus : '<?php echo $pagestatus; ?>'},
-		// 	target : [6],
-		// 	sort : '0'
-		// };
-		
-		// ajaxdatatables('.datatables', options);
-		
+				
 		validation(
 			'.form',
 			{
