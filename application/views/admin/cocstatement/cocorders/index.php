8f694d7d020da6f70a46235b043612c84a3d401b
manikandank228 <mr.manikandan01@gmail.com>
2020-03-27 18:11:17 +0530

    orders

--- application/views/admin/cocstatement/cocorders/index.php
+++ application/views/admin/cocstatement/cocorders/index.php
@@ -130,7 +130,7 @@ $tracking_display = ($delivery_type=='' || $delivery_type=='1') ? 'displaynone' 
 						</div>
 					</div>
 
-					<div class="row">						
+					<div class="row coc-perm-req">						
 						<div class="col-md-6">
 							<div class="form-group">
 								<label>Number of COC's Permitted to be allocated</label>
