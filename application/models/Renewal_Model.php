7b6af1ee8b9724239c7e8179b0080217fbef0191
manikandank228 <mr.manikandan01@gmail.com>
2020-03-27 13:13:54 +0530

    renewaL

--- application/models/Renewal_Model.php
+++ application/models/Renewal_Model.php
@@ -22,7 +22,7 @@ class Renewal_Model extends CC_Model
 		}
 		
 		if(isset($requestdata['order']['0']['column']) && isset($requestdata['order']['0']['dir'])){
-			$column = ['inv.inv_id'];
+			$column = ['inv.inv_id', 'inv.created_at', 'ud.name', 'ud.surname', 'up.registration_no', 'inv.description', 'inv.total_cost', 'ud.status'];
 			$this->db->order_by($column[$requestdata['order']['0']['column']], $requestdata['order']['0']['dir']);
 		}
 
