8ddc3f6207d8285a52b356902ead65af14da8efd
Ashok-PIRB <Ashok-PIRB>
2020-04-27 13:39:59 +0530

    to implement the desgins

--- application/views/admin/administration/managearea/index.php
+++ application/views/admin/administration/managearea/index.php
@@ -18,13 +18,13 @@
 
 <div class="row page-titles">
 	<div class="col-md-5 align-self-center">
-		<h4 class="text-themecolor">Manage Area</h4>
+		<h4 class="text-themecolor">Managearea</h4>
 	</div>
 	<div class="col-md-7 align-self-center text-right">
 		<div class="d-flex justify-content-end align-items-center">
 			<ol class="breadcrumb">
 				<li class="breadcrumb-item"><a href="j<?php echo base_url().'admin/dashboard'; ?>">Home</a></li>
-				<li class="breadcrumb-item active">Manage Area </li>
+				<li class="breadcrumb-item active">Managearea </li>
 			</ol>
 		</div>
 	</div>
@@ -34,76 +34,65 @@
 	<div class="col-12">
 		<div class="card">
 			<div class="card-body">
-				<h4 class="card-title">Manage Area </h4>
+				<h4 class="card-title">Managearea </h4>
 				<?php if($checkpermission){ ?>
-					<form class="mt-4 form" action="" method="post">
-						 <div class="form-group col-6">
-							<div class="form-group">
+				<form class="mt-4 form" action="" method="post">
+					<div class="row">
+						<div class="form-group col-6">
+						 	<div class="form-group">
 							<label for="id">Province*</label>
 							<?php echo form_dropdown('province', $provincelist, $province, ['id' => 'province', 'class' => 'form-control province_name']); ?>
-							</div>
+	                        </div>
 						</div>
-						<?php if($iddata==''){ ?>
-							<div class="form-group col-6">
-								<input type="radio" class="radio-control box1" id="choice1" name="choice1" value="choice">
-								<label class="radio-button">New city</label>
-							
-								<input type="radio" class="radio-control" id="choice1" name="choice1" value="other" checked="checked" >
-								<label class="radio-button">Existing city</label>
-							</div>
-						<?php } ?>
-						<div class="existing-city" >
-							<div class="form-group col-6">
+	                    <div class="form-group col-6 mt_40">
+	                        <input type="radio" class="radio-control box1" id="choice1" name="choice1" value="choice">
+	                        <label class="radio-button">New city</label>
+						
+	                        <input type="radio" class="radio-control" id="choice1" name="choice1" value="other" checked="checked" >
+	                        <label class="radio-button">Existing city</label>
+						</div>
+					</div>
+
+					<div class="existing-city" >
+						<div class="row">
+		                    <div class="form-group col-6">
 								<label for="city">City</label>
-								<?php echo form_dropdown('city', [], $city, ['id' => 'city', 'class' => 'form-control city_name']); ?>
+		                        <?php echo form_dropdown('city', [], $city, ['id' => 'city', 'class' => 'form-control city_name']); ?>
 							</div>
 							<div class="form-group col-6">
 								<label class="Subrub">Suburb</label>
-								<input type="text" class="form-control suburb" id="suburb" name="suburb" value="<?php echo $suburb; ?>" placeholder="suburb">
+		                        <input type="text" class="form-control suburb" id="suburb" name="suburb" value="<?php echo $suburb; ?>" placeholder="suburb">
 							</div>
 						</div>
-						<div class="form-group col-6 new-city" style="display:none;">
-							<label class="city1">City</label>
-							<input type="text" class="form-control" id="city1" name="city1" value="<?php echo $suburb; ?>" placeholder="Please enter the city">
-						</div>
+					</div>
+					<div class="form-group col-6 new-city" style="display:none;">
+						<label class="city1">City</label>
+                        <input type="text" class="form-control" id="city1" name="city1" placeholder="Please enter the city">
+					</div>
 
-						<div class="row ">
-							<div class="col-md-6">
-								<div class="custom-control custom-checkbox mr-sm-2 mb-3 pt-2">
-									<input type="checkbox" class="custom-control-input" name="status" id="status" <?php if($status=='1')echo 'checked'; ?> value="1">
-									<label class="custom-control-label" for="status">Active</label>
-								</div>
-							</div>
-							<div class="col-md-6 text-right">
-								<input type="hidden" name="id" value="<?php echo $id; ?>">
-								<button type="submit" name="submit" value="submit" class="btn btn-primary"><?php echo $heading; ?></button>
+					<div class="row ">
+						<div class="col-md-6">
+							<div class="custom-control custom-checkbox mr-sm-2 mb-3 pt-2">
+								<input type="checkbox" class="custom-control-input" name="status" id="status" <?php if($status=='1')echo 'checked'; ?> value="1">
+								<label class="custom-control-label" for="status">Active</label>
 							</div>
 						</div>
-					</form>
-				<?php } ?>
-				<h4 class="card-title">Suburb</h4>
-				<div class="table-responsive">
+						<div class="col-md-6 text-right">
+							<input type="hidden" name="id" value="<?php echo $id; ?>">
+							<button type="submit" name="submit" value="submit" class="btn btn-primary"><?php echo $heading; ?> Area </button>
+							
+						</div>
+					</div>
+				</form>
+			<?php } ?>
+								<div class="table-responsive mt_20">
 					<table class="table table-bordered table-striped datatables fullwidth">
 						<thead>
 							<tr>
 								<th>Province</th>
 								<th>City</th>
 								<th>Suburb</th>
-								<th>Status</th>
-								<th></th>
-							</tr>
-						</thead>
-					</table>
-				</div>
-				
-				<h4 class="card-title">City</h4>
-				<div class="table-responsive">
-					<table class="table table-bordered table-striped fullwidth citytable">
-						<thead>
-							<tr>
-								<th>Province</th>
-								<th>City</th>
-								<th>Status</th>
+								<th>Active</th>
 								<th></th>
 							</tr>
 						</thead>
@@ -116,9 +105,6 @@
 </div>
 <div class="suburb"></div>
 <script>
-	var citydata = '<?php echo $citydata; ?>';
-	var iddata = '<?php echo $iddata; ?>';
-	
 	$(function(){
 		
 		citysuburb(['#province','#city'], ['<?php echo $city; ?>']);
@@ -133,25 +119,11 @@
 							{ "data": "action" }
 						],
 
-						target	:	[3,4],
+						target	:	[4],
 						sort	:	'0'
 		};
 		
 		ajaxdatatables('.datatables', options);
-		
-		var options1 = {
-			url 	: 	'<?php echo base_url()."admin/administration/managearea/managearea/DTCity"; ?>',
-			columns : 	[
-							{ "data"	: 	"provincename" },
-                            { "data" 	:	"name" },
-                            { "data"	: 	"status" },	
-							{ "data"	: 	"action" }
-						],
-			target	:	[3],
-			sort	:	'0'
-		};
-		
-		ajaxdatatables('.citytable', options1);
 		
 		validation(
 			'.form',
@@ -187,46 +159,36 @@
 
 			}
 		);
-		
-		if(citydata!=''){
-			$(".existing-city").hide();
-			$(".new-city").show();
-		}
 	});
-	
+$("input[type='radio']").change(function(){
+if($(this).val()=="other")
+{
+$(".new-city").hide();
+}else{
+	$(".new-city").show();
+}
+});
 	$("input[type='radio']").change(function(){
-		if($(this).val()=="other")
-		{
-			$(".new-city").hide();
-		}
-		else
-		{
-			$(".new-city").show();
-		}
-	});
-	
-	$("input[type='radio']").change(function(){
-		if($(this).val()=="choice")
-		{
-			$(".existing-city").hide();
-		}
-		else
-		{
-			$(".existing-city").show(); 
-		}
-	});
+if($(this).val()=="choice")
+{
+$(".existing-city").hide();
+}
+else
+{
+$(".existing-city").show(); 
+}
+});
 
 	$('.province_name').on('change', function(){
 		citysuburb(['<?php echo base_url()."ajax/index/ajaxcity"; ?>', {provinceid : $(this).val()}, '#city']);	
 	});
 	
+	// Delete
 	
 	$(document).on('click', '.delete', function(){
-		var city	=	($(this).attr('data-city')) ? $(this).attr('data-city') : 0;
 		var action 	= 	'<?php echo base_url().'admin/administration/managearea/managearea'; ?>';
 		var data	= 	'\
 							<input type="hidden" value="'+$(this).attr('data-id')+'" name="id">\
-							<input type="hidden" value="'+city+'" name="citydata">\
 							<input type="hidden" value="2" name="status">\
 						';
 		sweetalert(action, data);
