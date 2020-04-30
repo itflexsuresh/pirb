d40e25717b1895296eadae917e5f1fd95a87a2f3
manikandanRJSM <rmanikandanrjsm@outlook.com>
2020-05-01 02:02:08 +0530

    pcoc

--- application/controllers/plumber/purchasecoc/Index.php
+++ application/controllers/plumber/purchasecoc/Index.php
@@ -757,8 +757,8 @@ class Index extends CC_Controller
            				$courierdetails = '<tr>
 					<td style="width: 50%;  margin: 0; padding: 10px 0 10px 5px;">Courier/Regsitered Post Fee</td>				
 					<td style="width: 10%;  margin: 0; padding: 10px 0 10px 0;text-align: center;"></td>
-					<td style="width: 19%; margin: 0; padding: 10px 0 10px 0;    text-align: center;">'.$currency2.$delivery_rate['amount'].'</td>
-					<td style="width: 18%;  margin: 0; padding: 10px 0 10px 0;    text-align: center;">'.$currency.$delivery_rate['amount'].'</td>
+					<td style="width: 19%; margin: 0; padding: 10px 0 10px 0;    text-align: center;">'.$currency2.$this->currencyconvertor($delivery_rate['amount']).'</td>
+					<td style="width: 18%;  margin: 0; padding: 10px 0 10px 0;    text-align: center;">'.$currency.$this->currencyconvertor($delivery_rate['amount']).'</td>
 					</tr>';
 
            			}
