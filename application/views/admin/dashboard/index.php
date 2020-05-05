012055f5419f662a3d4af94779af94d781846ff9
manikandanRJSM <rmanikandanrjsm@outlook.com>
2020-05-05 20:04:53 +0530

    dashboard

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -1,9 +1,24 @@
-<ol class="breadcrumb">
-	<li class="breadcrumb-item active">Dashboard</li>
-</ol>
-<div class="row">
+<div class="row page-titles">
+	<div class="col-md-5 align-self-center">
+		<h4 class="text-themecolor">Dashboard</h4>
+	</div>
+	<div class="col-md-7 align-self-center text-right">
+		<div class="d-flex justify-content-end align-items-center">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">Home</li>
+			</ol>
+		</div>
+	</div>
+</div>
+
+
+<div class="row">	
 	<div class="col-12">
-		<h1>Dashboard</h1>
-		<p>Welcome</p>
+		<div class="card">
+			<div class="card-body">
+				<h4 class="card-title">Dashboard</h4>	
+				<p>Welcome</p>
+			</div>
+		</div>
 	</div>
 </div>
