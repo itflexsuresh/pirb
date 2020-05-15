ae90161ffbf4aa910b612758cbbde3cd1323631c
manikandanRJSM <rmanikandanrjsm@outlook.com>
2020-05-15 16:34:23 +0530

    layout

--- application/views/template/layout2.php
+++ application/views/template/layout2.php
@@ -39,8 +39,8 @@
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<link href="<?php echo base_url().'assets/css/custom.css?vers=2.0'; ?>" rel="stylesheet">
-		<script src="<?php echo base_url().'assets/plugins/jquery/jquery-3.2.1.min.js'; ?>"></script>
+		<link href="<?php echo base_url().'assets/css/custom.css?version=3.0'; ?>" rel="stylesheet">
+		<script src="<?php echo base_url().'assets/plugins/jquery/jquery-3.2.1.min.js?version=3.0'; ?>"></script>
 	</head>
 
 	<body class="skin-blue fixed-layout">
