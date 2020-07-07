b0ecc5938b9fbd7fe0354ef1c27c1d363b157ce8
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-07-07 18:58:31 +0530

    api updated

--- application/controllers/webservice/Api.php
+++ application/controllers/webservice/Api.php
@@ -726,7 +726,7 @@ class Api extends CC_Controller
 			$jsonData['agreement'] = [ 'header' => ["I ".$jsonData['result']['u_name'].", Licensed registration number ".$jsonData['result']['plumberregno'].", certify that, the above compliance certifcate details are true and correct and will be logged in accordance with the prescribed requirements as defned by the PIRB. Select either A or B as appropriate"],'agreement1' => ['description' => 'A: The above plumbing work was carried out by me or under my supervision, and that it complies in all respects to the plumbing regulations, laws, National Compulsory Standards and Local bylaws.', 'agreementid' => '1'], 'agreement2' => ['description' => 'B: I have fully inspected and tested the work started but not completed by another Licensed plumber. I further certify that the inspected and tested work and the necessary completion work was carried out by me or under my supervision- complies in all respects to the plumbing regulations, laws, National Compulsory Standards and Local bylaws.', 'agreementid' => '2'], 
 			];
 
-			print_r($jsonData);die;
+			//print_r($jsonData);die;
 
 			if (count($jsonData['result']) > 0) {
 				$jsonArray = array("status"=>'1', "message"=>'View CoC', "result"=>$jsonData);
