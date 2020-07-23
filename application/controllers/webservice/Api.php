c3026ced98ca300bb54049c31d579e85e1b29e53
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-07-23 19:34:24 +0530

    selva mani

--- application/controllers/webservice/Api.php
+++ application/controllers/webservice/Api.php
@@ -1675,7 +1675,7 @@ class Api extends CC_Controller
 			$id = $this->input->post('COCno');
 			$userdata = $this->Coc_Model->getCOCList('row', ['id' => $id]);
 			if(!empty($userdata)){
-				if($userdata['coc_status'] == '4'){
+				if($userdata['coc_status'] == '4' && $userdata['coc_status'] == '2'){
 					$jsonData['pName']  = $userdata['u_name'];
 					$jsonData['pRegNo'] = $userdata['plumberregno'];
 					$jsonArray = array("status"=>'1', "message"=>'Plumber Detail', "result"=> $jsonData);
